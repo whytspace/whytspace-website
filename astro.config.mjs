34ad8d3d98dd3f4e0c41cfefa6a08f1cc8369c83
David H <dev@davidhell.com>
2024-08-31 10:10:34 +0200

    csp: never inline scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
+  /**
+   * Never inline scripts for to get rid of the 'unsafe-inline' CSP.
+   * Styles are never inlined by astro anyway
+   *
+   * @link https://github.com/withastro/roadmap/discussions/377#discussioncomment-8918763
+   */
+  vite: {
+    build: { assetsInlineLimit: 0 },
+  },
 });
